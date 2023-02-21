2bb6548ce67590a62d65dd87be12959fd9cdebef
Yurovskikh <yurovskikh.yasmin@yandex.ru>
2023-02-22 00:34:54 +0300

    refactoring: Setup to Webpack

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-import {
-    places,
-    cardTemplate,
-    buttonEdit,
-    buttonAdd,
-    imageForm,
-    placeForm,
-    profileForm,
-    name,
-    about,
-    title,
-    link,
-    nameInput,
-    jobInput,
-    initialCards,
-} from './constants.js';
+import './styles/index.css';
+
+const places = document.querySelector('.places');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
+
+const imageForm = document.querySelector('.image-popup');
+const placeForm = document.querySelector('.add-place-popup');
+const profileForm = document.querySelector('.edit-profile-popup');
+
+const name = document.querySelector('.popup__input_type_name');
+const about = document.querySelector('.popup__input_type_about');
+const title = document.querySelector('.popup__input_type_title');
+const link = document.querySelector('.popup__input_type_link');
+
+const nameInput = document.querySelector('.profile__name');
+const jobInput = document.querySelector('.profile__status');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 
 function openPopup(popup) {
     popup.classList.add('popup_opened');
